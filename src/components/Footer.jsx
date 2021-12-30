ac238336fcbbd4083fbc3d4b8ebffa76c6d1e4d9
TUFutz <adelinanastase2013@gmail.com>
2021-12-30 16:37:19 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,9 +12,10 @@ export function Footer() {
                     <span className="footer-copy">©proiect Ejobs 2021 </span>
                 </div>
                 <div className="footer-icons">
-                    <a href="https://github.com/Adelina0507><img src="images/github.svg" alt="github" /></a>
-                    <a href="https://www.facebook.com/nastase.adelina7"><img src="assets/images/facebook.svg" alt="facebook" /></a>
-                    <a href=https://www.linkedin.com/in/adelina-nastase-803842225/><img src="assets/images/linkedin.svg" alt="linkedin" /></a>
+                    <a href="https://github.com/Adelina0507"><img src="public/github.svg" alt="github" /></a>
+                    <a href="https://www.facebook.com/nastase.adelina7"><img src="public/facebook.svg" alt="facebook" /></a>
+                    <a href="https://www.linkedin.com/in/adelina-nastase-803842225/"><img src="public/linkedin.svg" alt="linkedin" /></a>
+
                 </div>
             </div>
         </footer>
